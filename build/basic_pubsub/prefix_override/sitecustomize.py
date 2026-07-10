6a121e485b29292b66a06dd4f649ae160603bd15
Edward <89059500+sfeng9@users.noreply.github.com>
2026-07-09 22:05:59 -0400

    Delete build directory

--- build/basic_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/Edward/workspaces/robotics-portfolio/ros2-vision-perception/install/basic_pubsub'
